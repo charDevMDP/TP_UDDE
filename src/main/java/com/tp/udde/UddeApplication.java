60e541e2e441c93f8566dbd984fbf7f15dc27bdf
charDevMDP <char.developer@gmail.com>
2021-04-27 22:59:12 -0300

    initial config sprintboot

---
+++ src/main/java/com/tp/udde/UddeApplication.java
@@ -0,0 +1,13 @@
+package com.tp.udde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UddeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UddeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/udde/UddeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.udde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UddeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
