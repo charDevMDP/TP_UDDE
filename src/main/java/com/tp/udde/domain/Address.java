4748bbcea63b95553dfe88bfbacdbdf7a43e41f5
Navarrodda <david95el@hotmail.com>
2021-04-29 00:03:06 -0300

    Config struct and create domains (City, User, Address)

---
+++ src/main/java/com/tp/udde/domain/Address.java
@@ -0,0 +1,42 @@
+package com.tp.udde.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Data
+@Builder
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "number")
+    private int number;
+
+    @Column(name = "department")
+    private int department;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(name="id_user")
+    private User users;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(name="id_city")
+    private  City city;
+}
